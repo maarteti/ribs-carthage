4bec328a26149f6fd12fe77f9a6845f5683a1eeb
maarteti <maarteti@gmail.com>
2021-03-20 23:45:16 +0900

    init commit

---
+++ ribs-carthage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ribs-carthage
+//
+//  Created by maartet on 2021/03/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .systemGray5
+    }
+
+
+}
+
